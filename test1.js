4f2471bf30b5f325877f51afcd8defd8bf1ecfe4
jacob f <jacob@jacobs-MBP.attlocal.net>
2020-06-09 09:33:32 -0700

    first

---
+++ test1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function isWhat(n) {
+  if (n < 2 || n % 1 !== 0) {
+    return false;
+  }
+  for (let i = 2; i < n; ++i) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+
+isWhat(5);
