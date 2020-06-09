cd82c434bab66bad9f355fb8d8f61a4ab93eea06
jacob f <jacob@jacobs-MBP.attlocal.net>
2020-06-09 11:46:01 -0700

    fini

--- test1.js
+++ test1.js
@@ -33,4 +33,18 @@ function stringReverser(string) {
   return newString;
 }
 
+function triangleCounter(n) {
+  //start at top add 1 then add 2 add 3
+  //n is the amount of layers in the triangle
+  //so at n = 3 total = 3 + 2 + 1 = 6
+  let total = 0;
+  for(let i = 0; i <= n; i++) {
+    total += n - i;
+    //n-0 = 3-0 = 3 total = 3
+    //n-1 = 3-1 = 2 total = 5
+    //n-2 = 3-2 = 1 total = 6
+  }
+  return total;
+}
+
 
